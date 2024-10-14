72fed3946c7f0896c554951d18f399adf11edd82
Yash Oswal <yoswal@redhat.com>
2024-10-14 19:07:48 +0530

    chore: enable guest sign-in in development env
    Signed-off-by: Yash Oswal <yoswal@redhat.com>

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -63,12 +63,15 @@ const app = createApp({
       <SignInPage
         {...props}
         auto
-        provider={{
-          id: 'github-auth-provider',
-          title: 'GitHub',
-          message: 'Sign in using GitHub',
-          apiRef: githubAuthApiRef,
-        }}
+        providers={[
+          'guest',
+          {
+            id: 'github-auth-provider',
+            title: 'GitHub',
+            message: 'Sign in using GitHub',
+            apiRef: githubAuthApiRef,
+          },
+        ]}
       />
     ),
   },
